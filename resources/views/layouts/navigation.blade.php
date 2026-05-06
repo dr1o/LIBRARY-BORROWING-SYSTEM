65f4a9ad65fd85e319f510e32e661aa9829b2b58
Muhammad Isya Asyhari <isyamuhammad341@gmail.com>
2026-05-06 20:41:16 +0700

    fix agar di home gaperlu login dulu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,3 @@
-<div class="bg-red-500 text-white p-2">
-    Role Anda Saat Ini: {{ Auth::user()->role }}
-</div>
-
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -19,22 +15,27 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @auth
+                        <div class="bg-red-500 text-white p-2">
+                            Role Anda Saat Ini: {{ Auth::user()->role }}
+                        </div>
+                        @if (Auth::user()->role == 'admin')
+                            <x-nav-link :href="route('equipments.index')" :active="request()->routeIs('equipments.*')">
+                                Kelola Alat (Admin)
+                            </x-nav-link>
+                            <x-nav-link href="#" :active="false">
+                                Laporan Pinjam
+                            </x-nav-link>
+                        @else
+                            <x-nav-link href="#" :active="false">
+                                Daftar Alat (Mahasiswa)
+                            </x-nav-link>
+                            <x-nav-link href="#" :active="false">
+                                Riwayat Pinjam
+                            </x-nav-link>
+                        @endif
+                    @endauth
 
-                    @if(Auth::user()->role == 'admin')
-                        <x-nav-link :href="route('equipments.index')" :active="request()->routeIs('equipments.*')">
-                            Kelola Alat (Admin)
-                        </x-nav-link>
-                        <x-nav-link href="#" :active="false">
-                            Laporan Pinjam
-                        </x-nav-link>
-                    @else
-                        <x-nav-link href="#" :active="false">
-                            Daftar Alat (Mahasiswa)
-                        </x-nav-link>
-                        <x-nav-link href="#" :active="false">
-                            Riwayat Pinjam
-                        </x-nav-link>
-                    @endif
                 </div>
             </div>
 
@@ -42,12 +43,19 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button
+                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            @auth
+                                
                             <div>{{ Auth::user()->name }}</div>
+                            @endauth
 
                             <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                        clip-rule="evenodd" />
                                 </svg>
                             </div>
                         </button>
@@ -63,7 +71,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -74,10 +82,14 @@
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -85,13 +97,15 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+@auth
+    
 
-            @if(Auth::user()->role == 'admin')
+            @if (Auth::user()->role == 'admin')
                 <x-responsive-nav-link :href="route('equipments.index')" :active="request()->routeIs('equipments.*')">
                     Kelola Alat (Admin)
                 </x-responsive-nav-link>
@@ -107,12 +121,15 @@
                 </x-responsive-nav-link>
             @endif
         </div>
-
+@endauth
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
+                @auth
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    
+                @endauth
             </div>
 
             <div class="mt-3 space-y-1">
@@ -125,7 +142,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
