ba907f44ef1fda46985c2c56c7d0f7a3e93ed934
dr1o <drio6077@gmail.com>
2026-06-02 16:15:40 +0700

    Configure trust proxies for Vercel edge network

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Force Laravel to trust Vercel's edge network proxies for secure links
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
